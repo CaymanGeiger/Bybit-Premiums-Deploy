d0f98707fb3d9425aa50fdbd7f395fb8dfcb14b4
Cayman Geiger <140837408+CaymanGeiger@users.noreply.github.com>
2024-01-07 20:47:58 -0700

    Test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
-// import styles from './page.module.css'
+import styles from './page.module.css'
 // import prisma from '../../lib/prisma'
-// import CoinFundingRates from './(components)/(funding)/CoinFundingRates'
-// import CoinBorrowRates from './(components)/(borrow)/CoinBorrowRates'
-// import Logo from './(components)/(logo)/Logo'
+import CoinFundingRates from './(components)/(funding)/CoinFundingRates'
+import CoinBorrowRates from './(components)/(borrow)/CoinBorrowRates'
+import Logo from './(components)/(logo)/Logo'
 
 // async function getCoinFundingRates() {
 //   const response = await prisma.coinFundingRate.findMany()
@@ -14,26 +14,26 @@
 //   return response;
 // }
 
-// export default async function Home() {
-//   const coinFundingRates = await getCoinFundingRates();
-//   const coinBorrowRates = await getCoinBorrowRates();
-//   console.log(coinFundingRates);
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.mainDivOne}>
-//         {/* <h1 className={styles.title}>
-//           Bybit Premiums
-//         </h1> */}
-//         <Logo />
-//       </div>
-//       <div className={styles.mainDivTwo}>
-//             <CoinFundingRates
-//               coinFundingRates={coinFundingRates}
-//             />
-//             <CoinBorrowRates
-//               coinBorrowRates={coinBorrowRates}
-//             />
-//       </div>
-//     </main>
-//   )
-// }
+export default async function Home() {
+  // const coinFundingRates = await getCoinFundingRates();
+  // const coinBorrowRates = await getCoinBorrowRates();
+  // console.log(coinFundingRates);
+  return (
+    <main className={styles.main}>
+      <div className={styles.mainDivOne}>
+        {/* <h1 className={styles.title}>
+          Bybit Premiums
+        </h1> */}
+        <Logo />
+      </div>
+      <div className={styles.mainDivTwo}>
+            {/* <CoinFundingRates
+              coinFundingRates={coinFundingRates}
+            />
+            <CoinBorrowRates
+              coinBorrowRates={coinBorrowRates}
+            /> */}
+      </div>
+    </main>
+  )
+}
