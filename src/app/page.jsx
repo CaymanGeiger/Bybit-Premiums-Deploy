ebc6fa83bf53ee800c6504a698ec6267a3537db4
Cayman Geiger <140837408+CaymanGeiger@users.noreply.github.com>
2024-01-10 22:45:48 -0700

    Test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,19 +4,23 @@ import CoinFundingRates from './(components)/(funding)/CoinFundingRates'
 import CoinBorrowRates from './(components)/(borrow)/CoinBorrowRates'
 import Nav from './(components)/(nav)/Nav'
 
+
 async function getCoinFundingRates() {
   const response = await prisma.coinFundingRate.findMany()
   return response;
 }
+
 
 async function getCoinBorrowRates() {
   const response = await prisma.coinBorrowRate.findMany()
   return response;
 }
 
+
 export default async function Home() {
   const coinFundingRates = await getCoinFundingRates();
   const coinBorrowRates = await getCoinBorrowRates();
+  
 
   // console.log(coinFundingRates);
   return (
