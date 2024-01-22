51b1b07ebc3b5d75b8d6994db20bf5ed5eef6d19
Cayman Geiger <140837408+CaymanGeiger@users.noreply.github.com>
2024-01-22 13:32:20 -0700

    Test:

--- src/app/(components)/(borrow)/CoinBorrowRates.jsx
+++ src/app/(components)/(borrow)/CoinBorrowRates.jsx
@@ -162,7 +162,7 @@ const CoinBorrowRates = ({ coinBorrowRates }) => {
                                             </button>
                                         </div>
                                     </th>
-                                    <th onClick={() => requestSort('spotVolume')}>24h Volume <strong className={styles.arrows}>{getSortIndicator('spotVolume')}</strong></th>
+                                    <th onClick={() => requestSort('spotVolume')}>Spot Volume <strong className={styles.arrows}>{getSortIndicator('spotVolume')}</strong></th>
                                     <th onClick={() => requestSort('oneDayAverage')}>1d <strong className={styles.arrows}>{getSortIndicator('oneDayAverage')}</strong></th>
                                     <th onClick={() => requestSort('threeDayAverage')}>3d <strong className={styles.arrows}>{getSortIndicator('threeDayAverage')}</strong></th>
                                     <th onClick={() => requestSort('sevenDayAverage')}>7d <strong className={styles.arrows}>{getSortIndicator('sevenDayAverage')}</strong></th>
@@ -170,6 +170,7 @@ const CoinBorrowRates = ({ coinBorrowRates }) => {
                                     <th onClick={() => requestSort('ninetyDayAverage')}>3m <strong className={styles.arrows}>{getSortIndicator('ninetyDayAverage')}</strong></th>
                                 </tr>
                             </thead>
+                            
                             <tbody>
                                 <AnimatePresence>
                                     {finalItemsToDisplay.map((coinBorrowRate) => {
