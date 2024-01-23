fa9f169f6de684ddf0f6131f8c52b3b0690e3822
Cayman Geiger <140837408+CaymanGeiger@users.noreply.github.com>
2024-01-22 20:39:29 -0700

    Test

--- src/app/(components)/(funding)/getFundingRates.js
+++ src/app/(components)/(funding)/getFundingRates.js
@@ -2,6 +2,6 @@
 
 const url = process.env.BACKEND_URL ? process.env.BACKEND_URL : process.env.NEXT_PUBLIC_BACKEND_URL
 export async function getCoinFundingRatesApi() {
-    const response = await fetch(`${url}/fundingrates`);
+    const response = await fetch(`${url}/fundingrates?timestamp=${new Date().getTime()}`);
     return response.json();
 }
