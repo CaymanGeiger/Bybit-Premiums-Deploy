ca821102d50788dc8ea0d0aa67973959ef300395
Cayman Geiger <140837408+CaymanGeiger@users.noreply.github.com>
2024-01-22 20:03:03 -0700

    Fixed Rates

--- src/app/(components)/(borrow)/getBorrowRates.js
+++ src/app/(components)/(borrow)/getBorrowRates.js
@@ -2,6 +2,6 @@
 
 const url = process.env.BACKEND_URL ? process.env.BACKEND_URL : process.env.NEXT_PUBLIC_BACKEND_URL
 export async function getCoinBorrowRatesApi() {
-    const response = await fetch(`${url}/borrowrates`);
+    const response = await fetch(`${url}/borrowrates?timestamp=${new Date().getTime()}`);
     return response.json();
 }
