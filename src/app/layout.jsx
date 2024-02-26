b65b9b9310a11d6df0f609cdcc38466b6a8bd413
Cayman Geiger <140837408+CaymanGeiger@users.noreply.github.com>
2024-02-25 17:06:59 -0700

    Added Script To Head Of Dom To Improve SEO

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,14 +8,29 @@ import Script from 'next/script'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Bybit Premiums',
-  description: 'All Your Stats In One Place',
-  url: 'https://bybitpremiums.com',
+  title: "Bybit Premiums - All Your Stats In One Place",
+  description:
+    "Explore comprehensive statistics and analytics for Bybit trading. Stay ahead with all your essential trading stats in one place. We display funding rates and borrows rates for the bybit market!",
+  type: "website",
+  url: "https://bybitpremiums.com",
   icons: {
-    icon: '/icon.png',
-  }
-}
+    icon: "/icon.png",
+  },
+};
 
+const structuredData = {
+  "@context": "http://schema.org",
+  "@type": "FinancialProduct",
+  "name": "Bybit Premiums - Funding and Borrow Rates",
+  "description": "Real-time Bybit funding and borrow rates for market traders.",
+  "provider": {
+    "@type": "Organization",
+    "name": "Bybit Premiums",
+    "url": "https://bybitpremiums.com"
+  }
+};
+
+const structuredDataString = JSON.stringify(structuredData);
 
 export default function RootLayout({children}) {
   return (
@@ -25,12 +40,12 @@ export default function RootLayout({children}) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Script
-        id='google_script1'
+        id="google_script1"
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.ANALYTICS_KEY}`}
       />
       <Script
-        id='google_script2'
+        id="google_script2"
         dangerouslySetInnerHTML={{
           __html: `
               window.dataLayer = window.dataLayer || [];
@@ -42,10 +57,14 @@ export default function RootLayout({children}) {
             `,
         }}
       />
+      <Script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: structuredDataString }}
+      ></Script>
       <body className={inter.className}>
         {children}
         <Toaster position="top-center" />
       </body>
     </html>
-  )
+  );
 }
