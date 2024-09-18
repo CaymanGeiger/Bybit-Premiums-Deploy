158d3b9d3d0ce9434bd57a3c49e97ef695c22e73
Cayman Geiger <140837408+CaymanGeiger@users.noreply.github.com>
2024-09-18 10:26:49 -0600

    Test 1234

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import Nav from "./(components)/(nav)/(bybit)/NavBybit";
 import Footer from "./(components)/(footer)/Footer";
 import Link from "next/link";
 
-// test
+// test 123
 let loadingData = true;
 const url = process.env.BACKEND_URL
   ? process.env.BACKEND_URL
